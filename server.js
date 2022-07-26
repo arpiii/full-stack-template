85a310f60392b6c3890e57d8272eb6b2d44634ff
arpi <coding.rp@gmail.com>
2022-07-26 15:49:12 -0400

    added endpoint in server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ MongoClient.connect(dbConnectionString)
 
 // Step 6
 app.set('view engine', 'ejs') // initializes and sets view engine to be ejs
+// app.set('views', './public/views') tells express where all views are..looks cleaner?
 app.use(express.static('public')) // let's the app automatically serve files in public as they are called upon (serving all the files from public if anyone access them under the base / path)
 app.use(express.urlencoded({extended: true})) // Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
 app.use(express.json()) // helps express parse json, pull it apart and extract data out of it. Allows us to read data passed back and forth
@@ -26,7 +27,13 @@ app.use(cors()) // allows cross-origin requests and stops cors errors in browser
 
 
 //The CRUD stuff goes here
-
+app.get('/', async (request, response) => {
+    try {
+        response.render('index.ejs')
+    } catch(error) {
+        response.status(500).send({message: error.message})
+    }
+})
 
 app.listen(process.env.PORT || PORT, () =>{
     console.log(`Server is running on port = ${process.env.PORT}`)
