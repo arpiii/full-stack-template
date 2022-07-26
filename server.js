32a2d6bd51651ae0aff0091e6655f789a27612f7
arpi <coding.rp@gmail.com>
2022-07-26 12:03:20 -0400

    Added variables, Mongo connections, and finalized instructions.txt

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient // The .MongoClient allows us to use the methods associated with mongoclient
 require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING, // Recommend this to be const instead so we get runtime errors if it's ever reassigned
+    dbName = 'star-trek-api',
+    collection
+
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to database')
+        db = client.db(dbName)
+        collection = db.collection('alien-info')
+    })
